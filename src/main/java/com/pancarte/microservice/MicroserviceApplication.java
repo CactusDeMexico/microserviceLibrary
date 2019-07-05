5e8f6b4ff63f97e664bf91742ba38c9d0beaa4d4
CactusDeMexico <33250840+CactusDeMexico@users.noreply.github.com>
2019-07-05 16:48:34 +0200

    add borrow and extended borrow

--- src/main/java/com/pancarte/microservice/MicroserviceApplication.java
+++ src/main/java/com/pancarte/microservice/MicroserviceApplication.java
@@ -1,19 +1,26 @@
 package com.pancarte.microservice;
 
+import com.pancarte.microservice.controller.EmailController;
+import com.pancarte.microservice.controller.LibraryControleur;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.test.context.ContextConfiguration;
+
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
+@EnableScheduling
 public class MicroserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceApplication.class, args);
+
 	}
 
 }
